20971f71a6a636b220b1d293b6cb70c4223914c5
Zhou Dangdang <31157683+RiDang@users.noreply.github.com>
2020-10-19 16:44:14 +0800

    Add files via upload

---
+++ models/transformer_base/__init__.py
@@ -0,0 +1,13 @@
+from .Constants import *
+from .Modules import *
+from .Layers import  EncoderLayer, DecoderLayer
+from .SubLayers import *
+from .Models import *
+from .Translator import *
+from .Beam import *
+from .Optim import *
+
+#__all__ = [
+#    transformer.Constants, transformer.Modules, transformer.Layers,
+#    transformer.SubLayers, transformer.Models, transformer.Optim,
+#    transformer.Translator, transformer.Beam]
---
+++ models/transformer_base/c__init__.py
@@ -0,0 +1,13 @@
+import models.transformer.Constants as Constants
+import models.transformer.Modules
+import models.transformer.Layers as Layers
+import models.transformer.SubLayers
+import models.transformer.Models
+import models.transformer.Translator
+import models.transformer.Beam
+import models.transformer.Optim
+
+__all__ = [
+    transformer.Constants, transformer.Modules, transformer.Layers,
+    transformer.SubLayers, transformer.Models, transformer.Optim,
+    transformer.Translator, transformer.Beam]
